e3410fcc5664e7e6c6f6b6cf83a19cd5913fa577
Allisson-Huilca <100052885+Alliss-H@users.noreply.github.com>
2022-03-04 11:24:18 -0500

    Create ejercicios asigancion_1.py

---
+++ Asignando valores a una variable/ejercicios asigancion_1.py
@@ -0,0 +1,32 @@
+# ejercicio 1
+print("Ejercicio 1")
+x = 15
+y = 25
+
+x = 100
+y = 225
+print(x)
+print(y)
+
+# ejercicio 2
+print("Ejercicio 2")
+a= 23
+b= 4
+c= 9
+
+a= 23
+b= 4
+c= 9
+print(a)
+print(b)
+print(c)
+
+# ejercicio 3
+print("Ejercicio 3")
+nombre= "Allisson"
+apellido= "Huilca"
+
+nombre= "Dayanna"
+apellido= "Lucio"
+print(nombre)
+print(apellido)
