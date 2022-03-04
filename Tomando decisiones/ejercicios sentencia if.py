b7f0850bcabcbf948bfba3a749e125edd82972ce
Allisson-Huilca <100052885+Alliss-H@users.noreply.github.com>
2022-03-04 11:37:25 -0500

    Create ejercicios sentencia if.py

---
+++ Tomando decisiones/ejercicios sentencia if.py
@@ -0,0 +1,8 @@
+# ejercicio sentencia if_1
+# Escribir un programa que solicite un valor entero al usuario y determine si es par o impar.
+
+num = int(input('Ingrese numero: '))
+if(num%2 == 0):
+    print('El número es par')
+else:
+    print('El número es impar')
