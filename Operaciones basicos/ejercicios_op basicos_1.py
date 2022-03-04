bd15c8eded8766535938d5dd0039f859583fd834
Allisson-Huilca <100052885+Alliss-H@users.noreply.github.com>
2022-03-04 11:03:48 -0500

    Create ejercicios_op basicos_1.py

---
+++ Operaciones basicos/ejercicios_op basicos_1.py
@@ -0,0 +1,31 @@
+# Ejercicios Operadores Basicos
+num1 = 40
+num2 = 16
+
+# suma
+suma= num1 + num2
+print(num1,"+", num2, "=", suma)
+
+# resta
+resta= num1 - num2
+print(num1,"-", num2, "=", resta)
+
+# multiplicacion
+multiplicacion= num1*num2
+print(num1,"x", num2, "=", multiplicacion)
+
+# division
+division = num1/num2                        
+print(num1,"/", num2, "=", division)
+
+# division (entera)
+divi_entera= num1//num2
+print(num1,"//", num2, "=", divi_entera)
+
+# modulo
+mod= num1%num2
+print(num1,"%", num2, "=", mod)
+
+# potencia
+potencia= num1**num2
+print(num1,"*", num2, "=", potencia)
