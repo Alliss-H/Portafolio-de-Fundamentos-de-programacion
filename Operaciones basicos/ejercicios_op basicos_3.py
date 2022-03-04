5a2df4260a918072dcd047f5ec8f199ae27c6263
Allisson-Huilca <100052885+Alliss-H@users.noreply.github.com>
2022-03-04 11:11:59 -0500

    Create ejercicios_op basicos_3.py

---
+++ Operaciones basicos/ejercicios_op basicos_3.py
@@ -0,0 +1,17 @@
+# Ejercicio_1
+print("Ejercicio 1")
+# Calcular el valor a cancelar por una deuda de $2000, considerando IVA del 14%. Imprima el valor a cancelar.
+deuda= 2000
+iva= 0.14
+valor_a_cancelar= (deuda  + (deuda * iva))
+print(" El valor a cancelar es: ", valor_a_cancelar)
+
+
+# Ejercicio_2
+print("Ejercicio 2")
+# Una tienda ofrece un descuentpo del 20% en la compra total a sus clientes.   
+precio= float(input("Ingrese el precio de su compra: "))
+descuento= precio*0.20
+precio_final= (precio - descuento) 
+"revisar"
+print("El precio final con el descuento es: ", precio_final)
