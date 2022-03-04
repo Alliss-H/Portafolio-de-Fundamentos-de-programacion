b37676013ee1ba0e87064ea84b8e16fced5c8d6d
Allisson-Huilca <100052885+Alliss-H@users.noreply.github.com>
2022-03-04 11:28:55 -0500

    Create ejercicios_nombrando_variable.py

---
+++ Nombrando una variable/ejercicios_nombrando_variable.py
@@ -0,0 +1,22 @@
+# ejercicio 1
+x = 15
+y = 25
+
+
+# ejercicio 2
+total= 330
+
+# ejercicio 3
+total_fruta= 12
+total_vegetales = 21
+
+
+# ejercicio 4
+anhio= 2022
+mes= "marzo"
+dia= 3
+
+
+# ejercicio 5
+color= "amarillo"
+animal= "perro
