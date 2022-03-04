47e2aace6893c49e56723eaf4629823b1838e26b
Allisson-Huilca <100052885+Alliss-H@users.noreply.github.com>
2022-03-04 12:30:27 -0500

    Update and rename Integer__1.py to Integer_2.py

---
+++ Tipos de datos en Python/Integer_2.py
@@ -0,0 +1,8 @@
+# Ejercicio Integer_2
+nombre= input("Ingrese su nombre:")
+apellido= input("Ingrse su apellido:")
+edad= input("Ingrese su edad:")
+
+print('Su nombre es:', nombre)
+print('Su apellido es:', apellido)
+print('Su edad es:',edad)
--- Tipos de datos en Python/Integer__1.py
+++
@@ -1,6 +0,0 @@
-# Ejercicio Integer_1
-pi= 3.1415
-radio= int(input("Ingrese el radio:"))
-
-area= pi*radio **2
-print('El area es:',area)
