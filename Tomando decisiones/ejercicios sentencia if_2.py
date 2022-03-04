e9b87250fee11fb7bb108bea3a74e1fe08d8b621
Allisson-Huilca <100052885+Alliss-H@users.noreply.github.com>
2022-03-04 11:38:34 -0500

    Create ejercicios sentencia if_2.py

---
+++ Tomando decisiones/ejercicios sentencia if_2.py
@@ -0,0 +1,11 @@
+# ejercicio sentencia if_2
+# Escribir un programa que solicite un valor entero al usuario y determine si es positivo o negativo.
+
+num = int(input("Ingrese numero: "))
+
+if num == 0:
+    print('Cero')
+elif num > 0:
+    print('Positivo')
+else:
+    print('Negativo')
