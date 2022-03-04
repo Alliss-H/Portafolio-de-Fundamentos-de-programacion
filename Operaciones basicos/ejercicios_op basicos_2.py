a2bf50144c2753a4a3bae50a1ed570da2bb8e28b
Allisson-Huilca <100052885+Alliss-H@users.noreply.github.com>
2022-03-04 11:09:43 -0500

    Create ejercicios_op basicos_2.py

---
+++ Operaciones basicos/ejercicios_op basicos_2.py
@@ -0,0 +1,35 @@
+# Ejercicios combinados de operadores basicos
+
+# ejercicio_1
+print("Ejercicio 1")
+a= 2
+resultado= (a+2)**3
+print("(2+2)**3", "=", resultado)
+
+# ejercicio_2
+print("Ejercicio 2")
+a= 4
+b= 3
+resultado2=(a+5)/(b-1)
+print("(4+5/(3-1)", "=", resultado2)
+
+# ejercicio_3
+print("Ejercicio 3")
+x=int(input("Ingrese un valor para x: "))
+resultado3= 1+x+(x*(x-1)*x**2)/2
+print("El resultado es: ", resultado3)
+
+# ejercicio_4
+print("Ejercicio 4")
+x=int(input("Ingrese un valor para x: "))
+resultado4= ((x+1)/(x+2))+(((x+5)*(x+1)**4)/(x+2))
+print("El resultado es: ", resultado4)
+
+# ejercicio_5
+print("Ejercicio 5")
+a=float(input("Ingrese un valor para a: " ))
+x=float(input("Ingrese un valor para x: " ))
+y=float(input("Ingrese un valor para y: " ))
+resultado5=(a**3 * (x**2 - 2*a*y))/(2*x)
+print("El resultado es : ", resultado5)
+
