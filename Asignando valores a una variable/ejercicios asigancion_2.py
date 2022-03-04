9c5f4287d5f852b1fdeb3827e92b37308d816c5e
Allisson-Huilca <100052885+Alliss-H@users.noreply.github.com>
2022-03-04 11:25:07 -0500

    Create ejercicios asigancion_2.py

---
+++ Asignando valores a una variable/ejercicios asigancion_2.py
@@ -0,0 +1,36 @@
+# ejercicio 1
+# Asignaciones en la misma línea
+print("Ejercicio 1")
+
+x= 12; y=10; z=4
+
+print('x=', x, 'y=', y, 'z=',z)
+
+
+# ejercicio 2
+# Asignación de intercambio
+print("Ejercicio 2")
+
+cocina= 300; cama= 55
+cocina, cama = cama, cocina
+
+print('cama: ', cama)
+print('cocina: ', cocina)
+# ejercicio 3
+# Asignación del mismo valor
+print("Ejercicio 3")
+
+tv = parlantes = 500
+print(tv + parlantes)
+
+# ejercicio 4
+# Asignación múltiple
+print("Ejercicio 4")
+
+day, month, year= "jueves", "marzo", 2022
+print("day:",day, "month:",month, "year:", year)
+
+
+# Ejercicio 5
+print("Ejercicio 5")
+print(sum([1,2,3,4]))
