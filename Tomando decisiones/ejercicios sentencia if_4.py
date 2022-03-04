efcfe4947e13812facf3ab48e82e20cfcf6cfbc3
Allisson-Huilca <100052885+Alliss-H@users.noreply.github.com>
2022-03-04 11:41:07 -0500

    Create ejercicios sentencia if_4.py

---
+++ Tomando decisiones/ejercicios sentencia if_4.py
@@ -0,0 +1,13 @@
+# ejercicio sentencia if_4
+#Calcular el mayor de tres números enteros introducidos por teclado.
+
+num1= int(input("Ingrese primer numero entero:"))
+num2= int(input("Ingrese segundo numero entero:"))
+num3= int(input("Ingrese tercer numero entero:"))
+# Calcular cual sera el dia siguiente
+dia = 4
+mes = 3
+anho = 2022
+print(dia, mes, anho)
+
+ultimo_dia = 28
